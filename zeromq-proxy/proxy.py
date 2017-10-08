03ccc97aa39255bff773b01ac7b9bf174ca24b05
Sergii Khomenko <sergii.khomenko@stylight.com>
2017-10-08 03:05:55 +0200

    Proper publishing for multiple ZMQ streams

--- zeromq-proxy/proxy.py
+++ zeromq-proxy/proxy.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 
 import zmq
 # import time
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 
 
 EGYM_ZMQ_SERVER = "tcp://35.195.199.160:5556"
@@ -12,33 +12,35 @@ CUSTOM_ZMQ = "tcp://127.0.0.1:5556"
 ANDROID_ZMQ = "tcp://*:5557"
 
 
-def server(android_server, custom_server):
+def server(android_server, publisher_queue):
     """Server with custom events."""
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind(android_server)
     print("Running Android ZMQ server {}".format(android_server))
 
-    socket_custom = get_publisher(custom_server)
     while True:
         # Wait for next request from client
         message = socket.recv()
+        publisher_queue.put(message)
+
         socket.send('pong')
-        print("Android Server: Received request %s" % message)
-        socket_custom.send(message)
+        print("Android Server: [%s]" % message)
 
 
-def get_publisher(server):
-    """Get a custom piublisher."""
+def publisher(custom_server, publisher_queue):
+    """A publisher jo join all the streams."""
     context = zmq.Context()
     socket_custom = context.socket(zmq.PUB)
 
-    socket_custom.bind(server)
+    socket_custom.bind(custom_server)
 
-    return socket_custom
+    while True:
+        message = publisher_queue.get()
+        socket_custom.send(message)
 
 
-def egym_client(origin_server, custom_server, topic_filter=""):
+def egym_client(origin_server, socket_custom, topic_filter=""):
     """Client to stream data to our proxy."""
     context = zmq.Context()
     socket_origin = context.socket(zmq.SUB)
@@ -47,19 +49,22 @@ def egym_client(origin_server, custom_server, topic_filter=""):
     print("Connecting to origin server {}".format(origin_server))
     socket_origin.connect(origin_server)
 
-    socket_custom = get_publisher(custom_server)
-
     print("Ready to process messages")
     while True:
         message = socket_origin.recv()
-        socket_custom.send(message)
+        publisher_queue.put(message)
 
         print("Proxied message: {}".format(message))
 
 
 if __name__ == "__main__":
-    # And pretened we have a custom ZMQ server for wearables
-    Process(target=server, args=(ANDROID_ZMQ, CUSTOM_ZMQ)).start()
+    publisher_queue = Queue()
 
-    # forward all original eGYM data to a custom ZMQ
-    # Process(target=egym_client, args=(EGYM_ZMQ_SERVER, CUSTOM_ZMQ)).start()
+    # Create a publisher process to let clients to subscribe
+    Process(target=publisher, args=(CUSTOM_ZMQ, publisher_queue)).start()
+
+    # Forward all original eGYM data to a custom ZMQ
+    Process(target=egym_client, args=(EGYM_ZMQ_SERVER, publisher_queue)).start()
+
+    # And pretend we have a custom ZMQ server for wearables
+    Process(target=server, args=(ANDROID_ZMQ, publisher_queue)).start()
